b049268f84a81c950edede70818f3e512a66afb9
Gregor Maclaine <thegregormaclaine@gmail.com>
2023-07-07 16:29:04 +0100

    Added p5js & JamLib starter

---
+++ src/sketch.js
@@ -0,0 +1,14 @@
+function preload() {}
+
+function mouseClicked() {}
+
+function keyPressed() {}
+
+function setup() {
+  createCanvas(800, 600);
+}
+
+function draw() {
+  cursor();
+  background(240);
+}

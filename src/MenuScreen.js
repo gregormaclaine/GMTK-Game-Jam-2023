d0772d7bcea2750d28d1777741cc7f1271428f25
Gregor Maclaine <thegregormaclaine@gmail.com>
2023-07-09 15:34:34 +0100

    Hooked up menu screen

--- src/MenuScreen.js
+++ src/MenuScreen.js
@@ -3,24 +3,57 @@ class MenuScreen {
     this.images = images;
     this.dialogue = dialogue;
     this.start_game = start_game;
+
+    this.in_credits = false;
+    this.credits = new JL.Credits(credits);
+
+    this.start_rect = [width * 0.5, height * 0.5, 450, 75];
+    this.credits_rect = [width * 0.5, height * 0.7, 450, 75];
   }
 
   handle_click() {
-    this.start_game();
+    if (this.in_credits) this.credits.handle_click();
+
+    if (this.mouse_over_rect(this.start_rect)) this.start_game();
+    else if (this.mouse_over_rect(this.credits_rect)) {
+      this.in_credits = true;
+      this.credits.start().then(() => (this.in_credits = false));
+    }
+  }
+
+  mouse_over_rect(rect) {
+    const [x, y, w, h] = rect;
+    if (mouseX < x - w / 2) return false;
+    if (mouseX > x + w / 2) return false;
+    if (mouseY < y - h / 2) return false;
+    if (mouseY > y + h / 2) return false;
+    return true;
   }
 
   show() {
+    if (this.in_credits) return this.credits.show();
+
     background(200);
     imageMode(CORNER);
     image(this.images['menu_bg'], 0, 0, 800, 600);
+
     imageMode(CENTER);
-    image(this.images['start-button'], width * 0.5, height * 0.5);
-    image(this.images['credits-button'], width * 0.5, height * 0.8);
+    image(this.images['start-button'], ...this.start_rect);
+    image(this.images['credits-button'], ...this.credits_rect);
 
     textSize(40);
     textAlign(CENTER);
-    text('Game where you play as fish.', width * 0.5, height * 0.25);
+    stroke(0);
+    strokeWeight(0);
+    fill(0);
+    text('Game where you play as fish.', width * 0.5, height * 0.3);
   }
 
-  update() {}
+  update() {
+    if (
+      this.mouse_over_rect(this.credits_rect) ||
+      this.mouse_over_rect(this.start_rect)
+    )
+      cursor('pointer');
+  }
 }
--- src/constants.js
+++ src/constants.js
@@ -5,4 +5,4 @@ const SHOW_HITBOXES = false;
 const credits = `
     fish PNG Designed By 1081**** from
     https://pngtree.com/freepng/fish-sea-fish-fish_8243296.html?sol=downref&id=bef
-`;
+`.split('\n');
